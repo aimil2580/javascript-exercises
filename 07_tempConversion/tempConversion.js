1afebf1e6f109902cedb4aab36ed691f32d392a8
aimil2580 <milidakisaimilios@gmail.com>
2024-07-15 15:19:45 +0300

    gg

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const CTemp = (temp-32)*(5/9);
+  return Math.round(CTemp * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const FTemp = (temp*1.8)+32;
+  return Math.round(FTemp * 10)/10;
 };
 
 // Do not edit below this line
