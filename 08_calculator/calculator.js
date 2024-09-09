afc052157fba551ccd0a2878987149cb29fb7a8c
aimil2580 <milidakisaimilios@gmail.com>
2024-09-10 00:21:06 +0300

    idk what i just did

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i != array.length; i++) {
+    sum = sum + array[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let sum = 1;
+  for (let i = 0; i != array.length; i++) {
+    sum = sum * array[i]
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const aplhanum = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let strCorrect = str.toLowerCase().split("").filter((char) => aplhanum.includes(char)).join('')
+    let strrev = strCorrect.split('').reverse().join('')
+    return strCorrect === strrev  
 };
 
 // Do not edit below this line
