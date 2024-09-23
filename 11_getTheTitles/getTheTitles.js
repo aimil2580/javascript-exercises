a7f6b4a65874780c5338f785030d8440796cf112
aimil2580 <milidakisaimilios@gmail.com>
2024-09-23 15:14:19 +0300

    im board

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titleArray = books.map((name) => name.title)
+    return titleArray    
 };
 
 // Do not edit below this line
