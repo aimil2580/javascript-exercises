064e07cf42b618f8cfdbc7ed47c942a0da67d51a
aimil2580 <milidakisaimilios@gmail.com>
2024-09-23 18:56:13 +0300

    lmao im stupid

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let sum=0
+    sum = number - 1 + number - 2
+    return sum
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let oldest = 0
+    let oldestObj
+    const date1 = new Date().getFullYear();
+    array.map((people) => {
+        if ("yearOfDeath" in people) {
+            if (parseInt(people.yearOfDeath - people.yearOfBirth) > oldest) {
+                oldestObj = people
+                oldest = parseInt(people.yearOfDeath - people.yearOfBirth)
+            }
+        } else {
+            if (parseInt(date1 - people.yearOfBirth) > oldest) {
+                oldestObj = people
+                oldest = parseInt(date1 - people.yearOfBirth)
+            }
+        }
+    }
+    )
+    return oldestObj
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
